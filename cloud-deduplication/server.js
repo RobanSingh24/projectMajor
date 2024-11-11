93f4c0648e649b1b5ae27454eb10e7292cd5236d
RobanSingh24 <106796911+RobanSingh24@users.noreply.github.com>
2024-11-11 10:00:06 +0530

    edit 6

--- cloud-deduplication/server.js
+++ cloud-deduplication/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: '*' // Allow all origins or specify your client URL here
+  origin: 'https://project-major-client.vercel.app/*' // Allow all origins or specify your client URL here
 }));
 app.use(express.json());
 
